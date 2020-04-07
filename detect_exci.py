360d64fe2adf7229b446b9d1ec379d88efff103e
levkeO <levke_ortlieb@wweb.de>
2020-04-07 09:52:42 +0100

    Got rid of a few bugs, still not working as intended

--- detect_exci.py
+++ detect_exci.py
@@ -9,6 +9,10 @@ sys.path.append(path)
 import singPartDist as sp
 from numba import njit, config, __version__
 from numba.extending import overload
+import numpy as np
+from scipy.interpolate import UnivariateSpline
+from scipy.optimize import curve_fit
+import pylab as pl
 
 def tanh(x,c1,t0,a,c2):
 	"""
@@ -49,12 +53,14 @@ def detect(fileName, tah, a, N,numFrames, L):
 	for particle in range(N):
 		p_coord = block[:,particle,:]
 		p_deltat = []
+		t0_temp = []
 		cond1 =sp.averageDistPos(p_coord,0,tah,-1-tah,-1,0,L) 
-		if cond > (2*a)**2:
+		if cond1 > (2*a)**2:
 			for frame in range(tah,numFrames-tah):
 				cond2 = sp.averageDistPos(p_coord,frame-tah,frame,frame,frame+tah,frame,L)
 				if cond2 > (2*a)**2:
-					p = sp.singpath(p_coord,frame)
+					p = sp.singlepath(p_coord,frame,L)
+					t = np.arange(p.diffs.shape[0])
 					w = np.append(0,np.where(p.diffs[frame-tah:frame+tah]-a<0)[0])
 					last = max(w)+1 # last time the distance drops below a/2	
 					first = min(w[w>0]) # last time in other direction the  distance drops below a/2
@@ -63,17 +69,22 @@ def detect(fileName, tah, a, N,numFrames, L):
 				if len(p_deltat)>0:
 					p_t0  = t0_temp[np.argmin(p_deltat)]
 					exPart.append(particle)
-					t_fit = t[t0-tah:t0+tah]
-					p_fit = p.diffs2[t0-tah:t0+tah]
+					t_fit = t[p_t0-tah:p_t0+tah]
+					p_fit = p.diffs[p_t0-tah:p_t0+tah]
 					spl =UnivariateSpline(t_fit,p_fit,s=0.8)
+					pl.figure()
+					pl.plot(t_fit,p_fit)
+					pl.plot(t_fit,spl(t_fit))
+					pl.title(particle)
 					try:
-						fit_param, covar = curve_fit(mytanh,t_fit,spl(t_fit),[tah/20,0.3,t0,0.5], bounds = ([0,-np.inf,0,-np.inf], [np.inf,np.inf,np.inf,np.inf]))
+						fit_param, covar = curve_fit(mytanh,t_fit,spl(t_fit),[tah/20,0.3,p_t0,0.5], bounds = ([0,-np.inf,0,-np.inf], [np.inf,np.inf,np.inf,np.inf]))
 					except:
 						print('Particle ',particle,'failed!')
 						break
 					deltat.append(fit_param[0]*2)
 					start.append(fit_max[0][0])		
 					t0.append(fit_param[2])
+	pl.show()
 	
 	return exPart, deltat, start, t0
 					
@@ -86,8 +97,8 @@ print(fileName[0])
 rho=1.4
 N = 10002
 L  = (N/rho)**(1./3.)
-a,b,c,d = detect(fileName[0],200,0.3,N,1000,L)
-print(min(deltat),len(deltat))
-
+a,b,c,d = detect(fileName[0],200,0.15,N,1000,L)
+#print(min(b),len(b))
+pl.show()
 
 
