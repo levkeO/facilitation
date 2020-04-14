790fd4947c5a2b76c19290e77b271461e8922dc4
levkeO <levke_ortlieb@wweb.de>
2020-04-14 14:59:26 +0100

    Added loop for many files and saving all results in pandas dataFrame

--- detect_exci.py
+++ detect_exci.py
@@ -13,8 +13,8 @@ import numpy as np
 from scipy.interpolate import UnivariateSpline
 from scipy.optimize import curve_fit
 import pylab as pl
-
-def tanh(x,c1,t0,a,c2):
+import pandas as pd
+def mytanh(x,c1,t0,a,c2):
 	"""
 	hyberbolic tangens with for parameters
 	
@@ -55,50 +55,46 @@ def detect(fileName, tah, a, N,numFrames, L):
 		p_deltat = []
 		t0_temp = []
 		cond1 =sp.averageDistPos(p_coord,0,tah,-1-tah,-1,0,L) 
-		if cond1 > (2*a)**2:
+		if cond1 > a**2:
 			for frame in range(tah,numFrames-tah):
 				cond2 = sp.averageDistPos(p_coord,frame-tah,frame,frame,frame+tah,frame,L)
-				if cond2 > (2*a)**2:
+				if cond2 > a**2:
 					p = sp.singlepath(p_coord,frame,L)
 					t = np.arange(p.diffs.shape[0])
-					w = np.append(0,np.where(p.diffs[frame-tah:frame+tah]-a<0)[0])
+					w = np.append(0,np.where(p.diffs[frame-tah:frame+tah]-a/2<0)[0])
 					last = max(w)+1 # last time the distance drops below a/2	
 					first = min(w[w>0]) # last time in other direction the  distance drops below a/2
 					p_deltat.append(last-first) # append deltat to particle array
 					t0_temp.append(frame)
-				if len(p_deltat)>0:
-					p_t0  = t0_temp[np.argmin(p_deltat)]
-					exPart.append(particle)
-					t_fit = t[p_t0-tah:p_t0+tah]
-					p_fit = p.diffs[p_t0-tah:p_t0+tah]
-					spl =UnivariateSpline(t_fit,p_fit,s=0.8)
-					pl.figure()
-					pl.plot(t_fit,p_fit)
-					pl.plot(t_fit,spl(t_fit))
-					pl.title(particle)
-					try:
-						fit_param, covar = curve_fit(mytanh,t_fit,spl(t_fit),[tah/20,0.3,p_t0,0.5], bounds = ([0,-np.inf,0,-np.inf], [np.inf,np.inf,np.inf,np.inf]))
-					except:
-						print('Particle ',particle,'failed!')
-						break
-					deltat.append(fit_param[0]*2)
-					start.append(fit_max[0][0])		
-					t0.append(fit_param[2])
-	pl.show()
-	
-	return exPart, deltat, start, t0
+			if len(p_deltat)>0:
+				print(particle)
+				p_t0  = t0_temp[np.argmin(p_deltat)]
+				p = sp.singlepath(p_coord,p_t0-tah,L)
+				exPart.append(particle)
+				t_fit = t[p_t0-tah:p_t0+tah]
+				p_fit = p.diffs[p_t0-tah:p_t0+tah]
+				spl =UnivariateSpline(t_fit,p_fit,s=0.8)
+				try:
+					fit_param, covar = curve_fit(mytanh,t_fit,spl(t_fit),[0.3,p_t0,tah/20,0.5], bounds = ([-np.inf,0,0,-np.inf], [np.inf,np.inf,np.inf,np.inf]))
+				except:
+					print('Particle ',particle,'failed!')
+					continue
+				deltat.append(fit_param[0]*2)
+				t0.append(fit_param[2])
+	return exPart, deltat, t0
 					
 
 
 
-
+allResults = pd.DataFrame()
 fileName = glob.glob('../Data/T0.5/*')
-print(fileName[0])
+print(fileName)
 rho=1.4
 N = 10002
 L  = (N/rho)**(1./3.)
-a,b,c,d = detect(fileName[0],200,0.15,N,1000,L)
-#print(min(b),len(b))
-pl.show()
-
+for fileIndex in fileName:
+	exPart,deltat,t0 = detect(fileName[fileIndex],200,0.3,N,1000,L)
+	print(len(exPart))
+	allResults[fileName[fileIndex]] = pd.Series([exPart,deltat,t0], index = ['exPart','deltat','t0'])
+print(allResults)
 
---
+++ exci_NN.py
@@ -0,0 +1,10 @@
+
+from ovito import dataset
+import numpy as np
+from ovito.vis import *
+from ovito.io import import_file
+from ovito.modifiers import *
+import pylab as pl
+from ovito.data import NearestNeighborFinder
+from ovito.data import CutoffNeighborFinder
+
